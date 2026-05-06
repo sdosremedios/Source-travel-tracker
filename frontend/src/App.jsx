2af50d7cd498ffbad0a07ac88bc854843fbe4848
Steven dos Remedios <steven@meetthere.com>
2026-05-06 12:28:58 -0700

    css tweak

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ import {
 import favicon from "./assets/favicon.png";
 
 export default function App() {
-  const appVersion = "0.4.16";
+  const appVersion = "0.4.17";
   // Navigation state
   const [activeScreen, setActiveScreen] = useState("tripList");
   const [selectedTripId, setSelectedTripId] = useState(null);
