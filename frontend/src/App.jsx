2daf79155cbec07195702062cb457b7613524ae9
Steven dos Remedios <steven@meetthere.com>
2026-07-09 12:10:01 -0700

    Fixed scrolling in markdown-text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,7 +39,7 @@ import {
 import favicon from "./assets/favicon.png";
 
 export default function App() {
-  const appVersion = "0.7.8b"; 
+  const appVersion = "0.8.0"; 
   // Navigation state
   const [activeScreen, setActiveScreen] = useState("tripList");
   const [selectedTripId, setSelectedTripId] = useState(null);
